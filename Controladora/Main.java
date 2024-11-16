c9580b59ec46bb45008a8a68719b658fd8b413e9
Angel Barboza <144569016+Mangelbarboza@users.noreply.github.com>
2024-11-15 21:59:37 -0600

    te quiero twin

--- Controladora/Main.java
+++ Controladora/Main.java
@@ -3,20 +3,22 @@ package Controladora;
 import Vista.MenuPrincipal;
 import Controladora.controladora;
 
-@SuppressWarnings("unused")   // Esto es para quitar las advertencias de que no se usa una clase
+@SuppressWarnings("unused") // Esto es para quitar las advertencias de que no se usa una clase
 
-public class Main {  
+public class Main {
+
+    // Beli feas
 
     public static void main(String[] args) {
-    
+
         // Iniciar interfaz
         MenuPrincipal menu = new MenuPrincipal();
         menu.setVisible(true);
-        
+
         // Crear controladora
-      
+
         controladora controller = new controladora(menu);
-       
+
     }
 
 }
