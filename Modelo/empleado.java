c78c4719535374bf72716b94969f3f6ea7575adf
Nai2203 <hnaidelyn19@gmail.com>
2024-11-18 11:34:09 -0600

    Cambio nai

--- Modelo/empleado.java
+++ Modelo/empleado.java
@@ -1,70 +1,71 @@
 package Modelo;
 
 public class Empleado {
-    // Atributos  para los cálculos
+    // Información personal
+    private String nombre;
+    private String cedula;
+
+    // Información laboral
+    private String puesto;
+    private String fechaIngreso;
+
+    // Atributos para cálculos 
     private double salarioDiario;
     private double salarioMensual;
+    private double salarioBruto; 
     private int aniosTrabajados;
     private int diasTrabajados;
     private double horasExtras;
     private boolean aplicaRenta;
 
-    
-    public Empleado(double salarioDiario, double salarioMensual, int aniosTrabajados, int diasTrabajados, double horasExtras, boolean aplicaRenta) {
+    // Constructor
+    public Empleado(String nombre, String cedula, String puesto, String fechaIngreso, 
+                    double salarioDiario, double salarioMensual, double salarioBruto, 
+                    int aniosTrabajados, int diasTrabajados, double horasExtras, boolean aplicaRenta) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.puesto = puesto;
+        this.fechaIngreso = fechaIngreso;
         this.salarioDiario = salarioDiario;
         this.salarioMensual = salarioMensual;
+        this.salarioBruto = salarioBruto;
         this.aniosTrabajados = aniosTrabajados;
         this.diasTrabajados = diasTrabajados;
         this.horasExtras = horasExtras;
         this.aplicaRenta = aplicaRenta;
     }
 
-    
-    public double getSalarioDiario() {
-        return salarioDiario;
-    }
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public void setSalarioDiario(double salarioDiario) {
-        this.salarioDiario = salarioDiario;
-    }
+    public String getCedula() { return cedula; }
+    public void setCedula(String cedula) { this.cedula = cedula; }
 
-    public double getSalarioMensual() {
-        return salarioMensual;
-    }
+    public String getPuesto() { return puesto; }
+    public void setPuesto(String puesto) { this.puesto = puesto; }
 
-    public void setSalarioMensual(double salarioMensual) {
-        this.salarioMensual = salarioMensual;
-    }
+    public String getFechaIngreso() { return fechaIngreso; }
+    public void setFechaIngreso(String fechaIngreso) { this.fechaIngreso = fechaIngreso; }
 
-    public int getAniosTrabajados() {
-        return aniosTrabajados;
-    }
+    public double getSalarioBruto() { return salarioBruto; }
+    public void setSalarioBruto(double salarioBruto) { this.salarioBruto = salarioBruto; }
 
-    public void setAniosTrabajados(int aniosTrabajados) {
-        this.aniosTrabajados = aniosTrabajados;
-    }
+    public double getSalarioDiario() { return salarioDiario; }
+    public void setSalarioDiario(double salarioDiario) { this.salarioDiario = salarioDiario; }
 
-    public int getDiasTrabajados() {
-        return diasTrabajados;
-    }
+    public double getSalarioMensual() { return salarioMensual; }
+    public void setSalarioMensual(double salarioMensual) { this.salarioMensual = salarioMensual; }
 
-    public void setDiasTrabajados(int diasTrabajados) {
-        this.diasTrabajados = diasTrabajados;
-    }
+    public int getAniosTrabajados() { return aniosTrabajados; }
+    public void setAniosTrabajados(int aniosTrabajados) { this.aniosTrabajados = aniosTrabajados; }
 
-    public double getHorasExtras() {
-        return horasExtras;
-    }
+    public int getDiasTrabajados() { return diasTrabajados; }
+    public void setDiasTrabajados(int diasTrabajados) { this.diasTrabajados = diasTrabajados; }
 
-    public void setHorasExtras(double horasExtras) {
-        this.horasExtras = horasExtras;
-    }
+    public double getHorasExtras() { return horasExtras; }
+    public void setHorasExtras(double horasExtras) { this.horasExtras = horasExtras; }
 
-    public boolean isAplicaRenta() {
-        return aplicaRenta;
-    }
-
-    public void setAplicaRenta(boolean aplicaRenta) {
-        this.aplicaRenta = aplicaRenta;
-    }
+    public boolean isAplicaRenta() { return aplicaRenta; }
+    public void setAplicaRenta(boolean aplicaRenta) { this.aplicaRenta = aplicaRenta; }
 }
