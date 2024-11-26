08a32c159170a59be90f96fc6497e85674340938
belindacrystal <belindacorea03@gmail.com>
2024-11-25 23:33:04 -0600

    JIJI

--- Modelo/Logic3.java
+++ Modelo/Logic3.java
@@ -3,12 +3,10 @@ package Modelo;
 public class Logic3 {
   
     public static double calcularAguinaldo(double salarioMensual, double horasExtrasMensuales, int mesesTrabajados) {
-        // Calcular el ingreso mensual total (salario base más horas extras)
+        // Calcular el ingreso mensual total
         double ingresoMensualTotal = salarioMensual + horasExtrasMensuales;
-        
         // Calcular los ingresos devengados en función de los meses trabajados
         double ingresosAnuales = ingresoMensualTotal * mesesTrabajados;
-    
         // Calcular el aguinaldo como la doceava parte de los ingresos anuales
         return ingresosAnuales / 12;
     }
